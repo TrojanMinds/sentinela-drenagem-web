dacf1dfe86aac9955f5b3c7eabaa3ec3d6c01899
Mateus <shakii876954@gmail.com>
2024-10-20 12:10:53 -0300

    [feat]:<Style>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,12 @@ export default {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
+      colors: {
+        BG: "#0B1D26",
+        Yellow:"#FBD784",
+        Blue: "#7EE3FC",
+        ButtonBlue: "#1B7CEB"
+      }
     },
   },
   plugins: [],

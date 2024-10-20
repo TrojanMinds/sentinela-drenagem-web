dffc37b580dbfb2df4d6d20f712ed555c5bf7ad5
Mateus <shakii876954@gmail.com>
2024-10-20 12:12:12 -0300

    [feat]:<lil upd>

--- src/app/_components/Home/nav.tsx
+++ src/app/_components/Home/nav.tsx
@@ -1,8 +1,13 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Nav() {
   return (
-    <div>nav</div>
+    <div className='absolute w-full px-[10%] flex items-center justify-center'>
+        <div className='absolute left-0 flex items-center justify-center'>
+            <Image src={"/svgs/logo.svg"} width={500} height={500} alt='logo'/>
+        </div>
+    </div>
   )
 }
 

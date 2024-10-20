eb5275db3b1528770f52cd144e33539daac45022
Mateus <shakii876954@gmail.com>
2024-10-20 20:49:35 -0300

    [feat]:<Error fixes> vercel error fix

--- src/app/_components/Dashboard/NotSignInPage.tsx
+++ src/app/_components/Dashboard/NotSignInPage.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 function NotSignInPage() {
   return (
     <div className='flex flex-col w-full h-[100vh] text-Yellow text-2xl items-center justify-center bg-BG gap-2'>
-        You don't have access to see this page, please sign in clicking on the Link below:
+        You don&apos;t have access to see this page, please sign in clicking on the Link below:
         <Link href={"/"} className='text-ButtonBlue underline'>Click here to go back</Link>
     </div>
   )
